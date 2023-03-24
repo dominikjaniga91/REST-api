a4d73e7450ed7d9a68b220f3648eeabe70f5b753
dominikjaniga91 <dominikjaniga91@gmail.com>
2023-03-24 16:21:43 +0100

    Use spring boot maven plugin

--- jmp-service-rest/src/main/java/com/epam/Application.java
+++ jmp-service-rest/src/main/java/com/epam/Application.java
@@ -2,13 +2,11 @@ package com.epam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author Dominik_Janiga
  */
 @SpringBootApplication
-//@EnableWebMvc
 class Application {
 
     public static void main(String[] args) {
